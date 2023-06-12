2527238ecf65157e5f8bb9a67462a99e8b036603
Yuri Margaryan <yuri@yuri.margaryan>
2023-06-12 12:07:16 +0400

    added unit_tests folder who contains the tests list

---
+++ unit_tests/create_data/tester.sh
@@ -0,0 +1,9 @@
+sum=$((100-14))
+return_data=return_data.txt
+echo $sum > $return_data
+diff $return_data currect_return.txt > diff 
+if [ -s "diff" ]; then
+    echo "FAIL" > result.txt
+else
+    echo "PASS" > result.txt
+fi
---
+++ unit_tests/save_data/tester.sh
@@ -0,0 +1,9 @@
+sum=$((5+2))
+return_data=return_data.txt
+echo $sum > $return_data
+diff $return_data currect_return.txt > diff 
+if [ -s "diff" ]; then
+    echo "FAIL" > result.txt
+else
+    echo "PASS" > result.txt
+fi
