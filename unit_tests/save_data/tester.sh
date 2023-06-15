73b4608469d4fc3d97db3f767b449b6c55346eae
Yuri Margaryan <yuri@yuri.margaryan>
2023-06-15 10:19:43 +0400

    modified tests

--- unit_tests/save_data/tester.sh
+++ unit_tests/save_data/tester.sh
@@ -1,9 +1,14 @@
-sum=$((5+2))
+test_types=("nightly" "weekly") 
+tickets=()
+sum=$((5-5))
 return_data=return_data.txt
-echo $sum > $return_data
-diff $return_data currect_return.txt > diff 
-if [ -s "diff" ]; then
-    echo "FAIL" > result.txt
+dirname=$(basename $(pwd))
+result_dir="../../unit_tests_results/$dirname"
+mkdir -p ${result_dir}
+echo $sum > ${result_dir}/${return_data}
+diff $result_dir/$return_data out.gout > ${result_dir}/diff 
+if [ -s "$result_dir/diff" ]; then
+    echo "FAIL" > $result_dir/result.txt
 else
-    echo "PASS" > result.txt
+    echo "PASS" > $result_dir/result.txt
 fi
